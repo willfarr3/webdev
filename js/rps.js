c6f8e9cf014013d671be32f0ffa4468307a0f932
willfarr3 <243915@portlandschools.org>
2023-10-31 12:43:48 -0400

    java started

--- js/rps.js
+++ js/rps.js
@@ -1,10 +1,25 @@
-// 1. get access to all html elements (show one, students the rest)
-// 2. demonstrate using console.log() to see results of code
-// 3. add eventListener buttons (show one, students do the rest)
-// 4. creating a function to handle the gameplay which will be called when 
-//    a button is clicked
-// 5. get user choice (passed in to function), update userImg
-// 6. get computer choice (create a function to handle this), update compImg
-// 7. discuss/show global vs local space
-// 8. decide who wins and update result (new function)
-// 9. Next steps?
+// the DOM = document object model
+const rockBtn = document.getElementById('rock')
+
+const paperbtn = document.getElementById('paper')
+
+const scissorbtn = document.getElementById('scissor')
+const results = document.querySelector('.result')
+
+const userImg = document.querySelector('.user-img')
+const compImg = document.querySelector('.comp-img')
+
+function rockbtnClick() {
+    .innertext=('clicked')
+}
+function paperbtnClick() {
+    .innertext=('clicked')
+}
+
+function scissorsbtnClick() {
+    .innertext=('clicked')
+}
+
+rockBtn.addEventListener('click', rockbtnClick)
+paperBtn.addEventListener('click', paperbtnClick)
+scissorsBtn.addEventListener('click',scissorsbtnClick)
