7372e6746fbd37635d1d8577e3e2502040916983
willfarr3 <243915@portlandschools.org>
2023-10-31 11:51:53 -0400

    fixing

---
+++ js/rps.js
@@ -0,0 +1,10 @@
+// 1. get access to all html elements (show one, students the rest)
+// 2. demonstrate using console.log() to see results of code
+// 3. add eventListener buttons (show one, students do the rest)
+// 4. creating a function to handle the gameplay which will be called when 
+//    a button is clicked
+// 5. get user choice (passed in to function), update userImg
+// 6. get computer choice (create a function to handle this), update compImg
+// 7. discuss/show global vs local space
+// 8. decide who wins and update result (new function)
+// 9. Next steps?
