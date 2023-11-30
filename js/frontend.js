166e66bd9b0c2cd792fe2f44861ed41d49cf062d
willfarr3 <243915@portlandschools.org>
2023-11-30 12:44:09 -0500

    added js functionality (not done yet)

---
+++ js/frontend.js
@@ -0,0 +1,6 @@
+const subBtn = document.getElementById('submit')
+const results = document.querySelector('results')
+
+subBtn.addEventListener('click', () =>{
+    results.innertext=('email given')
+})
