d4ad503b5ac81e5ce6114a1a4a6409a16053bc08
willfarr3 <243915@portlandschools.org>
2024-03-08 11:39:03 -0500

    e

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
-const subBtn = document.getElementById('personal-info')
+const stockPrice = document.querySelector(".output")
+const url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes?region=US&symbols=AMD%2CIBM%2CAAPL';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '83aa7e4915msha45fa05c01ec122p187bfajsn2e1e98827135',
+		'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+	}
+};
 
 
+async function getPrice() {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result.symbol);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-
-subBtn.addEventListener('mouseover', () => {
-    subBtn.style.display=""
-}) 
+getPrice()
