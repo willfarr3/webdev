6266ad8f0b2c61a29ecc8b2ed35a88c9be0e9f6f
willfarr3 <243915@portlandschools.org>
2024-03-20 11:41:06 -0400

    awegwwr

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const theJoke = document.querySelector(".output")
 const temp = document.querySelector(".temp")
+
  "900b4b96ca474c8b59bd9bb846c16ba7"
+
 const jokeButton = document.querySelector(".joke-button")
 const url = 'https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes';
 const options = {
@@ -11,6 +13,32 @@ const options = {
 	}
 };
 
+const DateTime = new Date()
+const month = DateTime.getMonth()
+const date = DateTime.getDate()
+const year = DateTime.getFullYear()
+const hour = DateTime.getHours()
+let minutes = DateTime.getMinutes()
+console.log(month,date,year,hour,minutes)
+let amPm = 'am'
+
+if (minutes < 10) {
+	minutes = '0' +minutes.toString
+}
+
+if (hour > 12) {
+	hour = '1' + hour 
+	amPm = 'pm'
+	hour = hour -12
+}
+
+if (hour >= 12) {
+
+}
+
+
+let time = `${hour}:${minutes}${amPm}`
+DateTime.textcontent = time
 async function getJoke() {
     
 
@@ -30,4 +58,4 @@ jokeButton.addEventListener('click', () =>{
 })
 
 
-https:api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// https:api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
--- js/replica.js
+++ js/replica.js
@@ -1,33 +1,33 @@
-const plusBtn = document.querySelectorAll('plus-btn')
-const minusBtn = document.querySelectorAll('minus-btn')
-const sideHdr = document.querySelectorAll('side-heading')
-const hamburgerBtn = document.querySelector('hamburger-button')
-const link = document.querySelector('link')
+// const plusBtn = document.querySelectorAll('plus-btn')
+// const minusBtn = document.querySelectorAll('minus-btn')
+// const sideHdr = document.querySelectorAll('side-heading')
+// const hamburgerBtn = document.querySelector('hamburger-button')
+// const link = document.querySelector('link')
 
 
-plusBtn.addEventListener('click', () => {
-    plusBtn.forEach(plusBtn => {
-        plusBtn.addEventListener('click', () => {
-            let index = parseint(plusBtn.id)
-            plusBtn[index].style.display='none'
-            minusBtn[index].style.display='block'
-            sideHdr[index].style.display='block'
-        })
-    })                           
-})
-minusBtn.addEventListener('click',() =>  {
-    plusBtn.forEach(minusBtn => {
-        plusBtn.addEventListener('click', () => {
-            let index = parseint(plusBtn.id)
-            plusBtn[index].style.display='block'
-            minusBtn[index].style.display='none'
-            sideHdr[index].style.display='none'
-        })
-    })                           
-})
+// // plusBtn.addEventListener('click', () => {
+//     plusBtn.forEach(plusBtn => {
+//         plusBtn.addEventListener('click', () => {
+//             let index = parseint(plusBtn.id)
+//             plusBtn[index].style.display='none'
+//             minusBtn[index].style.display='block'
+//             sideHdr[index].style.display='block'
+//         })
+//     })                           
+// // })
+// minusBtn.addEventListener('click',() =>  {
+//     plusBtn.forEach(minusBtn => {
+//         plusBtn.addEventListener('click', () => {
+//             let index = parseint(plusBtn.id)
+//             plusBtn[index].style.display='block'
+//             minusBtn[index].style.display='none'
+//             sideHdr[index].style.display='none'
+//         })
+//     })                           
+// })
 
 
 
-hamburgerBtn.addEventListener('click', () => {
-    link.style.display='none'
-})
+// hamburgerBtn.addEventListener('click', () => {
+//     link.style.display='none'
+// })
